afdedccd7e41bd736367cab6084ec7816196ba46
patnir <rahul20patni@gmail.com>
2016-07-24 19:42:23 -0400

    Initial setup

---
+++ sentimentClassification.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 24 19:32:01 2016
+
+@author: Rahul Patni
+"""
+
+# Sentiment classifications
+
+#functions to write
+#
+#loadData
+#
+#threshold
+#
+#compare
+#
+#optimization algorithm
+#
+#check against testing data
+#
+#define weights datastructure
