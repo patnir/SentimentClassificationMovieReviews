cebfcc071525d006353a1cf8f3de29cddfa54c40
patnir <rahul20patni@gmail.com>
2016-07-24 22:31:25 -0400

    Loaddata function

--- sentimentClassification.py
+++ sentimentClassification.py
@@ -20,3 +20,18 @@ Created on Sun Jul 24 19:32:01 2016
 #check against testing data
 #
 #define weights datastructure
+
+def loadData(words):
+    fptr = open("words.txt")
+    for line in fptr:
+        words.append(str(line.rstrip()))
+    return
+    
+    
+def main():
+    words = []
+    loadData(words)
+    print words
+    
+if __name__ == "__main__":
+    main()
