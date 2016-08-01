84c4696d65032a15c00c7ccbff3c9c1bc914beb1
patnir <rahul20patni@gmail.com>
2016-08-01 19:11:08 -0400

    fixing dot product matrix dimension mismatch error

--- sentimentClassification.py
+++ sentimentClassification.py
@@ -26,6 +26,7 @@ def loadData(words):
     fptr = open("words.txt")
     for line in fptr:
         words.append(str(line.rstrip()))
+    fptr.close()
     return
     
 def printArray(X):
@@ -45,6 +46,7 @@ def determinePresence(filename, words):
             j += 1
     # add an additional feature that is always on to x
     X[len(words)] = 1
+    fptr.close()
     return X
 
 def setFeatures(X, words, dirname, Y):
@@ -60,7 +62,7 @@ def setFeatures(X, words, dirname, Y):
   
 def determineValidationPresence(filename, words):
     fptr = open(filename)
-    X = numpy.zeros(len(words) + 1)
+    X = numpy.zeros(len(words))
     j = 0
     for line in fptr:
         line = line.split(" ")
@@ -73,7 +75,7 @@ def determineValidationPresence(filename, words):
           
 def setValidationFeatures(X, words, dirname, Y):
     for i in os.listdir(dirname):
-        if "pos" in dirname:
+        if "Pos" in dirname:
             Y.append(1)
         else:
             Y.append(0)
@@ -92,7 +94,7 @@ def threshold(weights, X):
     # return 1 is the value is greater than the threshold
     # 1 if this indicates a positive review
     # 0 if it indicates a negative review
-    T = weights[len(X) - 1]
+    T = weights[len(weights) - 1]
     result = numpy.dot(weights, numpy.transpose(X))
     if result >= T:
         return 1
@@ -107,12 +109,15 @@ def training(X, words, weights, Y, k):
                     weights = numpy.subtract(weights, X[i])
                 else: 
                     weights = numpy.add(weights, X[i])
+                print weights
 
 def testing(words, weights):
     X = []
     Y = []
-    setValidationFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/pos/validation", Y)
-    setValidationFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/neg/validation", Y)
+    setValidationFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/training/validationNegSmall", Y)
+    setValidationFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/training/validationPosSmall", Y)
+#    setValidationFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/pos/validation", Y)
+#    setValidationFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/neg/validation", Y)
     new_weights = []
     T = weights[len(X) - 1]
     [new_weights.append(weights[i]) for i in range(len(weights) - 1)]
@@ -123,6 +128,7 @@ def testing(words, weights):
             result = 1
         else:
             result = 0
+        print i, "result", result
         if result == Y[i]:
             totalCorrect += 1.0
     print "Percentage correct is"
@@ -136,16 +142,13 @@ def main():
     #weights = initializeWeights(words)
     X = []
     Y = []
-#    setFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/training/posSmall", Y)
-#    setFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/training/negSmall", Y)
-    setFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/pos", Y)
-    setFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/neg", Y)
-    print len(X)
-    print len(X[0])
+    setFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/training/negSmall", Y)
+    setFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/training/posSmall", Y)
+#    setFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/pos", Y)
+#    setFeatures(X, words, "mix20_rand700_tokens_cleaned/tokens/neg", Y)
     weights = initializeWeights(words)
-    print len(weights)
     # number of repetitions
-    k = 100
+    k = 10
     training(X, words, weights, Y, k)
     testing(words, weights)
     
