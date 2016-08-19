0f1d7de1947cd2e8f424fd54ff391543b4e09dd5
patnir <rahul20patni@gmail.com>
2016-08-19 07:55:41 -0400

    Plotting accuracies as iterations increase

--- sentimentClassification3.py
+++ sentimentClassification3.py
@@ -10,6 +10,7 @@ Created on Thu Aug 18 23:56:04 2016
 import os
 import random
 import numpy
+import matplotlib.pyplot as plt
 
 def printArray(array):
     for i in range(len(array)):
@@ -69,7 +70,64 @@ def setFeaturesRandomly(words, X, Y, dirNeg, dirPos, isTraining):
             j += 1
             Y.append(1)
     return
-  
+
+def initializeWeights(words):
+    weights = numpy.zeros(len(words) + 1)
+    # the original weight vector, with Threshold added
+    weights[len(words)] = 0
+    return weights
+
+def threshold(weights, X):
+    T = weights[len(weights) - 1]
+    result = numpy.dot(weights, numpy.transpose(X))
+    if result >= T:
+        return 1
+    return 0
+
+def accuracyCheck(weights, T, X, Y):
+    totalCorrect = 0.0
+    for i in range(len(X)):
+        result = numpy.dot(weights, numpy.transpose(X[i]))
+        if result >= T:
+            result = 1
+        else:
+            result = 0
+        #print i, "result", result
+        if result == Y[i]:
+            totalCorrect += 1.0
+    print "Accuracy:",
+    accuracy = (float(totalCorrect) / float(len(Y))) * 100
+    print accuracy, "%"
+    return accuracy
+
+def settingWeights(words, xTrain, yTrain, xTest, yTest):
+    weights = initializeWeights(words)
+    k = 100
+    accuracies = []
+    xPlot = []
+    total = 0
+    for j in range(k):
+        print "for k = ", k
+        for i in range(len(xTrain) - 1):
+            result = threshold(weights, xTrain[i]) 
+            if result != yTrain[i]:
+                if result == 1:
+                    weights = numpy.subtract(weights, xTrain[i])
+                else: 
+                    weights = numpy.add(weights, xTrain[i])
+        T = weights[len(weights) - 1]
+        new_weights = []
+        [new_weights.append(weights[x]) for x in range(len(weights) - 1)]
+        accuracies.append(accuracyCheck(new_weights, T, xTest, yTest))
+        xPlot.append(total)
+        total += 1
+    plotData(xPlot, accuracies)
+    return
+
+def plotData(x, y):
+    plt.plot(y)
+    plt.show()
+
 def main():
     words = []
     loadWords(words)
@@ -83,7 +141,9 @@ def main():
     xTest = []
     yTest = []
     setFeaturesRandomly(words, xTest, yTest, dirNegTesting, dirPosTesting, False)
+    settingWeights(words, xTrain, yTrain, xTest, yTest)
     print "done setting features"
+    
     # then set weights
     
     
